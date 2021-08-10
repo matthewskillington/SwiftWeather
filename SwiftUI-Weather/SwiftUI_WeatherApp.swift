d6c7d666a3fd179839913ade0e2e563f5b19ff4b
Matthew Skillington <matthewskillington14@gmail.com>
2021-08-10 09:41:21 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Matthew Skillington on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
